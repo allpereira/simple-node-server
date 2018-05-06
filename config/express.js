b01ac7115ebfe8a4777c8f8055d60eff5cca16ea
All Pereira <eu@allpereira.com.br>
2018-05-06 13:10:15 -0300

    Changing folder public to application

--- config/express.js
+++ config/express.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('./public'));
-
+app.use(express.static('./application'));
 app.all('/*', function(req, res) { res.sendFile(path.resolve('application/index.html')); });
 
 module.exports = app;
+
