5948119ef0f527b33cc1dbff35a33d70f49b0aff
All Pereira <eu@allpereira.com.br>
2018-05-11 16:37:48 -0300

    Add Server file to run as Linux Server

--- server.service.js
+++ server.service.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 var http = require('http');
 var app = require('./config/express');
 
 http.createServer(app).listen(8888, function() {
-    console.log('Servidor escutando na porta: ' + this.address().port);
+    console.log('Frontend Server Running in ' + this.address().port+' port.');
 });
